c8b694bddb8f1eda00bf2927ff32dd985bba98fa
Supratim Mandal <supratimm98@gmail.com>
2026-05-28 11:52:36 +0530

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -58,7 +58,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Supratim Mandal. All rights reserved.
+          © 2026 Supratim Mandal. All rights reserved.
         </p>
       </div>
     </footer>
