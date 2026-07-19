43f18226e267f4db51c69ad36436427920315fb0
Supratim Mandal <supratimm98@gmail.com>
2026-07-20 01:09:51 +0530

    CSS changed

--- src/components/CodingSection/CodingSection.jsx
+++ src/components/CodingSection/CodingSection.jsx
@@ -7,9 +7,17 @@ const CodingSection = () => {
   return (
     <div
       id="coding"
-      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-2 "
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-2"
     >
-      <div className="flex flex-wrap justify-center items-center gap-6 mb-20">
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">CODING PORTFOLIO</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          A collection of coding journey across various platforms
+        </p>
+      </div>
+
+      <div className="flex flex-wrap justify-center items-center gap-6 mb-17">
         {codingStats.map((item) => (
           <CodingStatCard
             key={item.id}
